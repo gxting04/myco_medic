fcf51f765fe0958d99bf9a396a33da8572688584
tinggx <gtin0007@student.monash.edu>
2024-12-17 21:35:58 +0800

    Initial commit

---
+++ src/home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import { SignInButton } from '@clerk/clerk-react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+
+function Home() {
+  return (
+    <div>
+        {/* Header */}
+        <Header/>
+        {/* Hero */}
+        <Hero/>
+    </div>
+  )
+}
+
+export default Home      
